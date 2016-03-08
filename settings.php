cb633f8c5aa4aa0a856fb79827ccea8536a204f5
Ping.X <netten2005@163.com>
2016-03-08 19:54:31 +0800

    使用CachedSvnSource

--- settings.php
+++ settings.php
@@ -5,9 +5,10 @@ $source = SourceFactory::create('LofsSource', 'lo', 'local');
 $source->setEncoding('GBK');
 $source->setBasedir('.');
 
-$source = SourceFactory::create('SvnSource', 'svn', 'github');
+$source = SourceFactory::create('CachedSvnSource', 'svn', 'github');
 $source->setEncoding('GBK');
 $source->setBasedir('https://github.com/xphh/HolloWiki/trunk');
 $source->setAuth('', '');
+$source->setExpired(60);
 
 ?>
